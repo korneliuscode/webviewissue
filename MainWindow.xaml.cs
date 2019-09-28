33250f7b2b2c3525dff9d457c9921bffbb3d28c1
korneliuscode <7031553+korneliuscode@users.noreply.github.com>
2019-09-28 18:12:59 +0200

    Initial checkin

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Fluent;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace webViewIssue
+{
+
+	public partial class MainWindow : RibbonWindow
+    {
+        
+        
+        public MainWindow()
+        {
+			// Ensure the current culture passed into bindings is the OS culture.
+			// By default, WPF uses en-US as the culture, regardless of the system settings.
+			FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+	
+			InitializeComponent();
+		}
+
+		
+
+
+	}
+}
